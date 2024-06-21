a10b127f4362fb07d87449bd310eb4da68d54b26
Ronny André Løkken <ronny.lokken@iteam.no>
2024-06-21 12:14:03 +0200

    Function: Write-Log

---
+++ Functions/Write-Log.ps1
@@ -0,0 +1,28 @@
+﻿function Write-Log {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Message,
+ 
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('DEBUG', 'INFO', 'WARN', 'ERROR')]
+        [string] $Severity = 'INFO',
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $LogFilePath
+    )
+    
+    $logDirectory = [System.IO.Path]::GetDirectoryName($LogFilePath)
+    if (-not (Test-Path -Path $logDirectory)) {
+        New-Item -Path $logDirectory -ItemType Directory -Force
+    }
+
+    if (!(Test-Path $LogFilePath)) {
+        New-Item -Path $LogFilePath -ItemType File -Force
+    }
+
+    $LogLine = "[$(Get-Date -f 'yyyy-MM-dd HH:mm:ss')] $Severity`t$Message"
+    Add-Content -path $LogFilePath -Value $LogLine
+}
