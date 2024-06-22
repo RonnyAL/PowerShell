9bddaae81c0bda8236da345a57fb9ce707a4f794
Ronny André Løkken <ronny.lokken@iteam.no>
2024-06-22 20:58:51 +0200

    Function: Get-ExternalIP

---
+++ Functions/Get-ExternalIP.ps1
@@ -0,0 +1,6 @@
+﻿function Get-ExternalIP {
+    return (Invoke-WebRequest ifconfig.me/ip).Content.Trim()
+}
+
+# (Optional) Enable alias "whatsmyip"
+# Set-Alias -Name 'whatsmyip' -Value 'Get-ExternalIP'
