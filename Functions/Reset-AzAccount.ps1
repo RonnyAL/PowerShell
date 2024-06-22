c5fe5c4b99e0372c9ffb63f892dccdb64e955f30
Ronny André Løkken <ronny.lokken@iteam.no>
2024-06-22 20:57:15 +0200

    Function: Reset-AzAccount

---
+++ Functions/Reset-AzAccount.ps1
@@ -0,0 +1,8 @@
+﻿function Reset-AzAccount {
+    $i = 0
+    while (Get-AzContext) {
+        $i++
+        $null = Disconnect-AzAccount
+    }
+    Write-Host "Disconnected $i Azure account$(if ($i -ne 1) {"s"})."
+}
